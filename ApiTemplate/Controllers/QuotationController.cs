abd7e1c4c42b6da981731df22ac32d2d0435a82e
Jan Minarovsky <jan.minarovsky@ips-ag.com>
2018-04-05 11:09:27 +0200

    quotation test2

---
+++ ApiTemplate/Controllers/QuotationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Services;
+using Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiTemplate.Controllers
+{
+    [Route("api/[controller]")]
+    public class QuotationController : ApiBaseController<Quotation>
+    {
+        public QuotationController(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+    
+    }
+}
---
+++ Entities/Quotation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Entity;
+
+namespace Entities
+{
+    public class Quotation : EntityBase
+    {
+        public string Company { get; set; }
+        public decimal Price { get; set; }
+    }
+}
